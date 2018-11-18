b06a7233c2026a03915c3ab2ee33912d8e8c3cdc
wolinzx <wolinzx@163.com>
2018-11-18 11:38:50 +0800

    add eslint

--- src/router/index.js
+++ src/router/index.js
@@ -13,61 +13,98 @@ import Message from '@/page/message/Message'
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-      // 默认跳转路由
-    { 
-      path: '*', 
-      redirect: '/Home',
-      meta: {
-        // index: 0,
-        isChild: false
-      }
-    },
-    {
-      path: '/Home',
-      component: Home,
-      meta: {
-        z_index: 1
-      },
-      children: [
-        {path: '/',name: 'Home', redirect: '/Home/Follow', meta: {z_index: 1}},
-        {path: '/Home/Follow',name: 'Follow', component: Follow, meta: {z_index: 1}},
-        {path: '/Home/Recommend',name: 'Recommend', component: Recommend, meta: {z_index: 1}},
-        {path: '/Home/Popular',name: 'Popular', component: Popular, meta: {z_index: 1}},
-      ]
-    },
-    {
-      path: '/Message',
-      name: 'Message',
-      component: Message,
-      meta: {
-        z_index: 3
-      }
-    },
-    {
-      path: '/Mine',
-      name: 'Mine',
-      component: Mine,
-      meta: {
-        z_index: 4
-      },
-      children: [
-        { path: '/Mine/Login',name: 'Login', component: Login, meta: {isChild: true} },
-        { 
-          path: '/Mine/Profile',
-          name: 'Profile', 
-          component: Profile, 
-          meta: {isChild: true},
-          children: [
-            { path: '/Mine/Profile/EditProfile',
-              name: 'EditProfile', 
-              component: EditProfile, 
-              meta: {isChild: true},
-            },
+	routes: [
+		// 默认跳转路由
+		{
+			path: '*',
+			redirect: '/Home',
+			meta: {
+				// index: 0,
+				isChild: false
+			}
+		},
+		{
+			path: '/Home',
+			component: Home,
+			meta: {
+				z_index: 1
+			},
+			children: [{
+					path: '/',
+					name: 'Home',
+					redirect: '/Home/Follow',
+					meta: {
+						z_index: 1
+					}
+				},
+				{
+					path: '/Home/Follow',
+					name: 'Follow',
+					component: Follow,
+					meta: {
+						z_index: 1
+					}
+				},
+				{
+					path: '/Home/Recommend',
+					name: 'Recommend',
+					component: Recommend,
+					meta: {
+						z_index: 1
+					}
+				},
+				{
+					path: '/Home/Popular',
+					name: 'Popular',
+					component: Popular,
+					meta: {
+						z_index: 1
+					}
+				},
+			]
+		},
+		{
+			path: '/Message',
+			name: 'Message',
+			component: Message,
+			meta: {
+				z_index: 3
+			}
+		},
+		{
+			path: '/Mine',
+			name: 'Mine',
+			component: Mine,
+			meta: {
+				z_index: 4
+			},
+			children: [{
+					path: '/Mine/Login',
+					name: 'Login',
+					component: Login,
+					meta: {
+						isChild: true
+					}
+				},
+				{
+					path: '/Mine/Profile',
+					name: 'Profile',
+					component: Profile,
+					meta: {
+						isChild: true
+					},
+					children: [{
+							path: '/Mine/Profile/EditProfile',
+							name: 'EditProfile',
+							component: EditProfile,
+							meta: {
+								isChild: true
+							},
+						},
 
-          ]
-        },
-      ]
-    }
-  ]
+					]
+				},
+			]
+		}
+	]
 })
